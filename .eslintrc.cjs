be9315c4dd4fc07bf9db0d6b0b84bc055989df44
Jace Babin <jbabin91@gmail.com>
2023-05-11 21:38:40 -0700

    Setup linting and formatting
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,64 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:unicorn/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.config.js', '*.config.ts'],
+      rules: {
+        'unicorn/prefer-module': 'off',
+      },
+    },
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.eslint.json',
+    sourceType: 'module',
   },
-}
+  plugins: ['react-refresh', 'simple-import-sort', 'sort-keys-fix'],
+  rules: {
+    '@typescript-eslint/consistent-type-definitions': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    'import/default': 'off',
+    'import/no-unresolved': 'off',
+    'react-refresh/only-export-components': 'warn',
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        ignoreCase: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+        shorthandFirst: true,
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'sort-keys-fix/sort-keys-fix': 'error',
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          camelCase: true,
+          kebabCase: true,
+          pascalCase: true,
+        },
+      },
+    ],
+    'unicorn/prevent-abbreviations': 'off',
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from 'node:path';
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import mkcert from 'vite-plugin-mkcert';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    checker({
+      eslint: {
+        lintCommand: 'eslint .',
+      },
+      typescript: true,
+    }),
+    mkcert({
+      source: 'coding',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    https: true,
+  },
+});
